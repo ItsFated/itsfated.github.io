edbd7e63dd08e0f2a7ae4f3a6a85bf89fb65af1c
ItsFated <384378433@qq.com>
2024-07-19 22:25:02 +0800

    优化GithubActions，使用secrets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker compose -f compose.yaml down
 mkdir -p ./{data,logs}
-./prepare.sh
+./prepare.sh itsfated.top 替换appId 替换appKey 替换repo_id 替换category_id
 npm install
 npm run build
 docker compose -f compose.yaml up -d
--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,12 @@
-sed -i 's#url: https://itsfated.github.io/#url: https://itsfated.top/#' _config.yml
-sed -i 's#https://itsfated.github.io/atom.xml#https://itsfated.top/atom.xml#' _config.butterfly.yml
+# 示例：
+# perpare.sh itsfated.top appId appKey repo_id category_id
+
+# 替换域名
+sed -i "s#url: https://itsfated.github.io/#url: https://$1/#" _config.yml
+sed -i "s#https://itsfated.github.io/atom.xml#https://$1/atom.xml#" _config.butterfly.yml
+
+# 替换配置
+sed -i "s/56a07c6f-5882-412b-b502-154543266a8b/$2/" _config.butterfly.yml
+sed -i "s/ca126f51-86c5-41c5-a45c-7b584082fbcc/$3/" _config.butterfly.yml
+sed -i "s/da657660-f72f-401a-bfcc-f87aa68b9997/$4/" _config.butterfly.yml
+sed -i "s/e7fcd585-59b1-4dac-a218-10dc1c585b10/$5/" _config.butterfly.yml
