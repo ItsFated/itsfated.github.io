81b989e5af1f231229407ea89aca4f4ae83eec4c
ItsFated <384378433@qq.com>
2024-07-29 20:51:33 +0800

    更新底部信息（A6）

--- prepare.sh
+++ prepare.sh
@@ -10,3 +10,6 @@ sed -i "s/56a07c6f-5882-412b-b502-154543266a8b/$2/" _config.butterfly.yml
 sed -i "s/ca126f51-86c5-41c5-a45c-7b584082fbcc/$3/" _config.butterfly.yml
 sed -i "s/da657660-f72f-401a-bfcc-f87aa68b9997/$4/" _config.butterfly.yml
 sed -i "s/e7fcd585-59b1-4dac-a218-10dc1c585b10/$5/" _config.butterfly.yml
+
+# 替换底部自定义字符串，如：备案号
+sed -i "s!348c3414-b358-4a50-be83-7d20fcb25770!$6!" _config.butterfly.yml
